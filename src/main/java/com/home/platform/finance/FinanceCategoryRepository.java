6ab8ea40496ac6c3ac644bd2722808f1490f22f3
yeah~ <skjb6996@naver.com>
2026-05-04 17:23:26 +0900

    feat : 가계부 리포지토리 추가
    - FinanceCategoryRepository에 사용자별 카테고리 조회 메서드 추가
    
    - FinanceCategoryRepository에 카테고리 중복 확인 메서드 추가
    
    - FinanceTxRepository에 기간 조회와 월별 합산 JPQL 메서드 추가

---
+++ src/main/java/com/home/platform/finance/FinanceCategoryRepository.java
@@ -0,0 +1,13 @@
+﻿package com.home.platform.finance;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FinanceCategoryRepository extends JpaRepository<FinanceCategory, Long> {
+    List<FinanceCategory> findByUserIdAndCatTypeOrderBySortOrderAsc(String userId, String catType);
+
+    List<FinanceCategory> findByUserIdOrderBySortOrderAsc(String userId);
+
+    boolean existsByUserIdAndCatTypeAndCatName(String userId, String catType, String catName);
+}
